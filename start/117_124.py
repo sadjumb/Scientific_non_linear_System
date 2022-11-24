31774669c448031a173d809cdd0c6afcc990496e
Nick_022 <43909257+Aristoteles123@users.noreply.github.com>
2022-11-24 16:16:46 +0300

    Add files via upload

---
+++ start/117_124.py
@@ -0,0 +1,112 @@
+import numpy as np
+import pysindy as ps
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from mpl_toolkits.mplot3d import Axes3D
+from scipy import sparse
+
+rho = 28.0
+sigma = 10.0
+beta = 8.0 / 3.0
+dt = 0.01
+
+def f(state, t):
+    x, y, z = state
+    return sigma * (y - x), x * (rho - z) - y, x * y - beta * z
+
+state0 = [1.0, 1.0, 1.0]
+time_steps = np.arange(0.0, 40.0, dt)
+
+x_train = odeint(f, state0, time_steps)
+
+radius = 0.6
+sparsity = 0.01
+input_dim = 3
+reservoir_size = 1000
+n_steps_prerun = 10
+regularization = 1e-2
+sequence = x_train
+
+weights_hidden = sparse.random(reservoir_size, reservoir_size, density=sparsity)
+eigenvalues, _ = sparse.linalg.eigs(weights_hidden)
+weights_hidden = weights_hidden / np.max(np.abs(eigenvalues)) * radius
+
+weights_input = np.zeros((reservoir_size, input_dim))
+q = int(reservoir_size / input_dim)
+for i in range(0, input_dim):
+    weights_input[i * q:(i + 1) * q, i] = 2 * np.random.rand(q) - 1
+
+weights_output = np.zeros((input_dim, reservoir_size))
+
+def initialize_hidden(reservoir_size, n_steps_prerun, sequence):
+    hidden = np.zeros((reservoir_size, 1))
+    for t in range(n_steps_prerun):
+        input = sequence[t].reshape(-1, 1)
+        hidden = np.tanh(weights_hidden @ hidden + weights_input @ input)
+    return hidden
+
+def augment_hidden(hidden):
+    h_aug = hidden.copy()
+    h_aug[::2] = pow(h_aug[::2], 2.0)
+    return h_aug
+
+hidden = initialize_hidden(reservoir_size, n_steps_prerun, sequence)
+hidden_states = []
+targets = []
+
+for t in range(n_steps_prerun, len(sequence) - 1):
+    input = np.reshape(sequence[t], (-1, 1))
+    target = np.reshape(sequence[t + 1], (-1, 1))
+    hidden = np.tanh(weights_hidden @ hidden + weights_input @ input)
+    hidden = augment_hidden(hidden)
+    hidden_states.append(hidden)
+    targets.append(target)
+
+targets = np.squeeze(np.array(targets))
+hidden_states = np.squeeze(np.array(hidden_states))
+
+weights_output = (np.linalg.inv(hidden_states.T@hidden_states + regularization * np.eye(reservoir_size)) @ hidden_states.T@targets).T
+
+def predict(sequence, n_steps_predict):
+    hidden = initialize_hidden(reservoir_size, n_steps_prerun, sequence)
+    input = sequence[n_steps_prerun].reshape((-1, 1))
+    outputs = []
+
+    for t in range(n_steps_prerun, n_steps_prerun + n_steps_predict):
+        hidden = np.tanh(weights_hidden @ hidden + weights_input @ input)
+        hidden = augment_hidden(hidden)
+        output = weights_output @ hidden
+        input = output
+        outputs.append(output)
+    return np.array(outputs)
+
+x_sim = predict(sequence, 4000)
+
+plt.figure(figsize=(6, 4))
+plt.plot(x_train[:4000, 0], x_train[:4000, 2], label="Ground truth")
+plt.plot(x_sim[:, 0], x_sim[:, 2],'--', label="Reservoir computing estimate")
+plt.plot(x_train[0, 0], x_train[0, 2], "ko", label="Initial condition", markersize=8)
+
+plt.legend()
+plt.show()
+
+def plot_dimension(dim, name):
+    fig = plt.figure(figsize=(9,2))
+    ax = fig.gca()
+    ax.plot(time_steps, x_train[:, dim])
+    ax.plot(time_steps, x_sim[:, dim], "--")
+    plt.xlabel("time")
+    plt.ylabel(name)
+    plt.draw()
+    plt.show()
+
+plot_dimension(0, 'x')
+plot_dimension(1, 'y')
+plot_dimension(2, 'z')
+
+
+
+
+
+
+
